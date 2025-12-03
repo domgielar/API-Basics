d4b218fc876e04b45c285202945aa1e1df01f5f2
Dominik Gielarowiec <dominikgielarowiec@gmail.com>
2025-12-03 12:24:21 -0500

    Went deeper into NBA API and searched through gamefind er to see 5 celtics games and whether they won or lost.

--- API_basics.py
+++ API_basics.py
@@ -1,5 +1,7 @@
 from nba_api.stats.static import teams
+from nba_api.stats.endpoints import leaguegamefinder
 import matplotlib.pyplot as pyplot
+import pandas as pd
 
 def one_dict(list_dict):
     keys=list_dict[0].keys()
@@ -10,4 +12,18 @@ def one_dict(list_dict):
     return out_dict
 
 nba_teams = teams.get_teams()
-print(nba_teams[0:3])
+#print(nba_teams[0:3])
+
+dict_nba_teams=one_dict(nba_teams)
+df_teams = pd.DataFrame(dict_nba_teams)
+
+df_celtics = df_teams[df_teams["nickname"]=="Celtics"]
+print(df_celtics)
+
+id_celtics = df_celtics[["id"]].values[0][0]
+print(id_celtics)
+
+gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=id_celtics)
+gamefinder.get_json()
+games = gamefinder.get_data_frames()[0]
+print(games.head())
