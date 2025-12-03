8839e620545f51da3bf02d57ecd0cfe09a150bc3
Dominik Gielarowiec <dominikgielarowiec@gmail.com>
2025-12-03 11:57:22 -0500

    Initial commit: learning how to handle REST api by implementing the NBA API to find data, reearranged teams dictionaries for easier handling

---
+++ API_basics.py
@@ -0,0 +1,13 @@
+from nba_api.stats.static import teams
+import matplotlib.pyplot as pyplot
+
+def one_dict(list_dict):
+    keys=list_dict[0].keys()
+    out_dict={key:[] for key in keys}
+    for dict_ in list_dict:
+        for key,value in dict_.items():
+            out_dict[key].append(value)
+    return out_dict
+
+nba_teams = teams.get_teams()
+print(nba_teams[0:3])
